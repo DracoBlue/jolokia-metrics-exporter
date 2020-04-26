d037b5f2c434d416a7391552a01f1a24f33ec3ef
DracoBlue <JanS@DracoBlue.de>
2020-04-26 20:22:46 +0200

    Added initial commit

---
+++ README.md
@@ -0,0 +1,123 @@
+# jolokia-metrics-exporter
+
+This project is similar to the official prometheus jmx_exporter from https://github.com/prometheus/jmx_exporter but does
+support fetching data from an external jolokia endpoint.
+
+## Usage
+
+To make jolokia-metrics-exporter work, you will need 4 environment variables set and a config.yaml at
+`/usr/src/app/config.yaml` or any other location, which you can set via the `CONFIG_YAML` environment variable.
+
+The `.env`:
+
+```
+JOLOKIA_URL=http://example.org/jolokia
+USERNAME=admin
+PASSWORD=admin
+CONFIG_YML=/usr/src/app/config.yaml
+```
+
+If you want to use docker-compose.yml to try it, use:
+
+```yaml
+version: "2.0"
+services:
+  jolokia-metrics-exporter:
+    image: dracoblue/jolokia-metrics-exporter
+    env_file:
+      - .env
+    ports:
+      - "9442:9442"
+```
+
+## Example config.yaml
+
+This file should work for most JVM installations and expose the values properly as metrics:
+
+```yaml
+  - pattern: java.lang<type=Threading><>(.+ThreadCount)
+    name: java_lang_threading$1
+    attrNameSnakeCase: true
+    type: GAUGE
+    help: Java's $1
+  - pattern: java.lang<type=Threading><>(.+ThreadCount)
+    name: java_lang_threading_thread_count
+    attrNameSnakeCase: true
+    type: GAUGE
+    labels:
+      $1: $0
+    help: Java's $1
+  - pattern: java.lang<type=Memory><>HeapMemoryUsage/(.+)
+    name: java_lang_heap_memory_usage_$1
+    attrNameSnakeCase: true
+    type: GAUGE
+    help: Heap memory $1 usage
+  - pattern: java.lang<type=Runtime><>SpecVersion
+    name: java_lang_runtime_spec_version
+    overrideValue: 1
+    labels:
+      "version": $0
+    type: GAUGE
+  - pattern: java.lang<name=PS Scavenge,type=GarbageCollector><>(CollectionCount|CollectionTime)
+    name: java_lang_garbage_collector$1
+    attrNameSnakeCase: true
+    filter:
+      Name: "PS Scavenge"
+    type: GAUGE
+    help: Index Queue Size
+```
+
+will give you an endpoint exposing the metrics, like this:
+
+```text
+# HELP java_lang_threading_total_started_thread_count Java's TotalStartedThreadCount
+# TYPE java_lang_threading_total_started_thread_count gauge
+java_lang_threading_total_started_thread_count 22139
+# HELP java_lang_threading_peak_thread_count Java's PeakThreadCount
+# TYPE java_lang_threading_peak_thread_count gauge
+java_lang_threading_peak_thread_count 154
+# HELP java_lang_threading_daemon_thread_count Java's DaemonThreadCount
+# TYPE java_lang_threading_daemon_thread_count gauge
+java_lang_threading_daemon_thread_count 88
+# HELP java_lang_threading_thread_count Java's TotalStartedThreadCount
+# TYPE java_lang_threading_thread_count gauge
+java_lang_threading_thread_count{TotalStartedThreadCount="22139"} 22139
+java_lang_threading_thread_count{PeakThreadCount="154"} 154
+java_lang_threading_thread_count{DaemonThreadCount="88"} 88
+# HELP java_lang_heap_memory_usage_init Heap memory init usage
+# TYPE java_lang_heap_memory_usage_init gauge
+java_lang_heap_memory_usage_init 494927872
+# HELP java_lang_heap_memory_usage_committed Heap memory committed usage
+# TYPE java_lang_heap_memory_usage_committed gauge
+java_lang_heap_memory_usage_committed 6716129280
+# HELP java_lang_heap_memory_usage_max Heap memory max usage
+# TYPE java_lang_heap_memory_usage_max gauge
+java_lang_heap_memory_usage_max 9544663040
+# HELP java_lang_heap_memory_usage_used Heap memory used usage
+# TYPE java_lang_heap_memory_usage_used gauge
+java_lang_heap_memory_usage_used 3754287416
+# TYPE java_lang_runtime_spec_version gauge
+java_lang_runtime_spec_version{version="1.8"} 1
+# HELP java_lang_garbage_collector_collection_time Index Queue Size
+# TYPE java_lang_garbage_collector_collection_time gauge
+java_lang_garbage_collector_collection_time 408073
+# HELP java_lang_garbage_collector_collection_count Index Queue Size
+# TYPE java_lang_garbage_collector_collection_count gauge
+```
+
+## Related projects
+
+* [jmx_exporter](https://github.com/prometheus/jmx_exporter)
+  - requires exposed RMI port
+  - no jolokia support
+  - doesn't allow overriding value for 1 in case of e.g. app version
+  - doesn't allow to filter a list
+* [jolokia_exporter](https://github.com/Scalify/jolokia_exporter) 
+  - no label support
+  - doesn't allow overriding value for 1 in case of e.g. app version
+  - doesn't allow setting type of targets
+  - doesn't allow to filter a list 
+
+## License
+
+This work is copyright by DracoBlue (http://dracoblue.net) and licensed under the terms of MIT License.
---
+++ index.js
@@ -0,0 +1,210 @@
+require('dotenv').config();
+const debug = require('debug')('jolokia-metrics-exporter');
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const YAML = require('yaml');
+const got = require('got').extend({
+  username: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  prefixUrl: process.env.JOLOKIA_URL
+});
+
+
+const configFileContent = fs.readFileSync(process.env.CONFIG_YML || '/opt/jolokia-metrics-exporter/config.yaml', 'utf8');
+const config = YAML.parse(configFileContent);
+
+debug('launch request');
+
+class Rule {
+  constructor({rawString, mbeanClass, mbeanPath, filter, labels, attribute, overrideValue, help, type, name, attrNameSnakeCase}) {
+    this.rawString = rawString;
+    this.mbeanClass = mbeanClass;
+    this.mbeanPath = mbeanPath;
+    this.attribute = attribute;
+    this.help = help;
+    this.type = type;
+    this.name = name;
+    this.attrNameSnakeCase = attrNameSnakeCase;
+    this.labels = labels || {};
+    this.filter = filter || {};
+    this.overrideValue = overrideValue;
+
+    if (this.attribute.indexOf('/') !== -1) {
+      this.path = this.attribute.split('/').slice(0, -1).join('/');
+      this.attribute = this.attribute.split('/').slice(-1).join('/');
+    } else {
+      this.path = undefined;
+    }
+    if (this.attribute.indexOf('(') !== -1) {
+      this.attributeRegExp = new RegExp(this.attribute);
+    } else {
+      this.attributeRegExp = null;
+    }
+  }
+
+  toJolokiaRequest() {
+    return {
+      "type": "read",
+      "mbean": this.mbeanClass + ":" + this.mbeanPath,
+      "attribute": this.path
+    }
+  }
+
+  getPrometheusLinesForJolokiaResponse(jolokiaResponse) {
+    let matchedValues = [];
+    let replaceValues = [];
+
+    let resultValues = [];
+    if (Array.isArray(jolokiaResponse.value)) {
+      resultValues = jolokiaResponse.value;
+    } else {
+      resultValues = [jolokiaResponse.value];
+    }
+
+    resultValues.forEach((resultValue) => {
+      if (this.filter) {
+        let isMatch = true;
+        Object.keys(this.filter).forEach((filterKey) => {
+          if (this.filter[filterKey] !== resultValue[filterKey]) {
+            isMatch = false;
+          }
+        })
+
+        if (!isMatch) {
+          return false;
+        }
+      }
+      if (this.attributeRegExp) {
+        Object.keys(resultValue).forEach((key) => {
+          let attributeMatch = this.attributeRegExp.exec(key);
+          debug('attributeMatch', attributeMatch, key, this.attributeRegExp);
+          if (attributeMatch) {
+            if (attributeMatch[0] === attributeMatch[1]) {
+              /* full match: (TheKey) */
+              let replacer = {};
+              replacer['$1'] = attributeMatch[1];
+              matchedValues.push({value: resultValue[key], replacer});
+            } else {
+              /* partial match: The(Part)OfTheKey */
+            }
+          }
+        });
+      } else {
+        if (typeof resultValue[this.attribute] !== undefined) {
+          matchedValues.push({value: resultValue[this.attribute]});
+        }
+      }
+    });
+    let replacePositionalParameters = (match, replacer) => {
+      return replacer[match] || 'undefined';
+    };
+    let snakeCase = (string) => {
+       string = string.charAt(0).toLowerCase() + string.slice(1);
+       return string.split(/(?=[A-Z])/).join('_').replace(/(\-)/g, '_').toLowerCase();
+    }
+
+    debug('matchedValues', matchedValues);
+    let isNameAlreadyDefinedMap = {};
+
+    let lines = [];
+    matchedValues.forEach((matchedValue) => {
+      matchedValue.replacer = matchedValue.replacer || {};
+      matchedValue.replacer['$0'] = matchedValue.value;
+          let name = this.name.replace(/(\$\d+)/, (match) => replacePositionalParameters(match, matchedValue.replacer));
+          if (this.attrNameSnakeCase) {
+             name = snakeCase(name);
+          }
+          if (!isNameAlreadyDefinedMap[name]) {
+            if (this.help) {
+              let help = this.help.replace(/(\$\d+)/, (match) => replacePositionalParameters(match, matchedValue.replacer));
+              lines.push('# HELP ' + name + ' ' + help);
+            }
+            lines.push('# TYPE ' + name + ' ' + this.type.toLowerCase());
+            isNameAlreadyDefinedMap[name] = true;
+          }
+          let labelsSuffixParts = [];
+          Object.keys(this.labels).forEach((sourceLabelKey) => {
+            let labelKey = sourceLabelKey.replace(/(\$\d+)/, (match) => replacePositionalParameters(match, matchedValue.replacer));
+            let labelValue = this.labels[sourceLabelKey].replace(/(\$\d+)/, (match) => replacePositionalParameters(match, matchedValue.replacer));
+            labelsSuffixParts.push([labelKey, '=', '"', labelValue, '"'].join(''));
+          });
+          let overrideValue = this.overrideValue;
+          if (typeof overrideValue === "undefined") {
+            overrideValue = matchedValue.value;
+          }
+          if (labelsSuffixParts.length > 0) {
+            lines.push(name + '{' + labelsSuffixParts.join(",") + '} ' + overrideValue);
+          } else {
+            lines.push(name + ' ' + overrideValue);
+          }
+    });
+    return lines;
+  }
+}
+
+const convertRuleConfigToRule = (ruleConfig) =>  {
+  let ruleOptions = {};
+  let patternMatch = /^([^<]+)<([^>]+)><>(.+)$/.exec(ruleConfig.pattern);
+  debug(patternMatch);
+  if (!patternMatch) {
+    throw new Error('Invalid pattern');
+  }
+  [rawString, mbeanClass, mbeanPath, attribute] = patternMatch;
+
+  return new Rule({rawString, mbeanClass, mbeanPath, attribute, overrideValue: ruleConfig.overrideValue, filter: ruleConfig.filter, labels: ruleConfig.labels, help: ruleConfig.help, name: ruleConfig.name, type: ruleConfig.type,
+attrNameSnakeCase: ruleConfig.attrNameSnakeCase || false});
+}
+
+let enableRequest = true;
+
+
+let rules = config.rules.map(convertRuleConfigToRule);
+rules.forEach((rule) => {
+  debug('rule', rule);
+});
+
+const getMetrics = () => {
+  return got.post('', {
+    json: rules.map((rule) => rule.toJolokiaRequest())
+  }).json().then((jolokiaResponse) => {
+    return new Promise((resolve, reject) => {
+    let lines = [];
+    debug('jolokiaRequest', rules.map((rule) => rule.toJolokiaRequest()));
+    debug('jolokiaResponse', jolokiaResponse);
+    rules.forEach((rule, pos) => {
+       debug('response', rule, jolokiaResponse[pos]);
+       rule.getPrometheusLinesForJolokiaResponse(jolokiaResponse[pos]).forEach((line) => {
+         debug('line', line);
+         lines.push(line);
+       });
+    });
+      resolve(lines);
+    });
+  });
+};
+
+if (enableRequest) {
+  getMetrics().then((lines) => {
+  });
+}
+
+setTimeout(() => {}, 60 * 1000 * 1000);
+
+
+app.get('/', function (req, res) {
+  res.set('X-App-Version', process.env.APP_VERSION || 'dev')
+  res.send('OK');
+});
+
+app.get('/metrics', function (req, res) {
+  res.set('X-App-Version', process.env.APP_VERSION || 'dev')
+  getMetrics().then((lines) => {
+    res.send(lines.join("\n"));
+  });
+});
+
+app.listen(process.env.PORT || 9442, '0.0.0.0', function () {
+});
+
+
